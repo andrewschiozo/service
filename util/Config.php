da9bc7c8142ab95a563cd99e58bce8a3dc5209a9
Andrews <andrewschiozo@gmail.com>
2023-11-03 15:23:12 -0300

    first commit

---
+++ util/Config.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace util;
+
+abstract class Config
+{
+	const SYSDB = ['user' => ''
+					,'pass' => ''
+					,'name' => ''
+					,'host' => ''
+					,'port' => ''
+					,'dsn' => ''
+					,'options' => []
+				];
+
+    const JWTKEY = '';
+}
---
+++ util/Request.php
@@ -0,0 +1,104 @@
+<?php
+namespace util;
+
+use controller;
+
+class Request
+{
+	const ALLOW_REQUEST_METHODS = ['POST', 'GET', 'PUT'];
+
+	private $url;
+	private static $data;
+	private static $header;
+	private static $token;
+	private static $method;
+	private static $resource;
+
+	public function __construct()
+	{
+		if($this->checkMethod() && $this->checkController())
+		{
+			$this->formatHeader();
+			self::$data = new \stdClass;
+			self::$token = str_replace('Bearer ', '', self::$header['Authorization']);
+			$controller = $this->formatController();
+			$method = strtoupper($_SERVER['REQUEST_METHOD']);
+			self::$method = $method;
+			$this->setData();
+			$controller = new $controller();
+			$controller->$method();
+		}
+		else
+		{
+			Response::getInstance()->badRequest("Requisição inválida");
+		}
+	}
+
+	private function checkMethod()
+	{
+		return in_array(strtoupper($_SERVER['REQUEST_METHOD']), self::ALLOW_REQUEST_METHODS);
+	}
+
+	private function checkController()
+	{
+		$controller = $this->formatController();
+		$filePath = str_replace('\\', DIRECTORY_SEPARATOR, $controller) . '.php';
+		return file_exists($filePath) && class_exists($controller);
+	}
+
+	private function formatHeader()
+	{
+		self::$header = apache_request_headers();
+		self::$header['Authorization'] = array_key_exists('Authorization', self::$header) ? self::$header['Authorization'] : '';
+	
+		return self::$header['Authorization'];
+	}
+
+	private function formatController()
+	{
+		$uri = explode('/' ,$_SERVER['REQUEST_URI']);
+		$uri = array_slice($uri, 3);
+		$uri = implode('\\', $uri);
+		$controller = strtok($uri, '?');
+		$controller = 'controller\\' . $controller;
+		return $controller;
+	}
+
+	private function setData()
+	{
+		if(array_key_exists('REDIRECT_QUERY_STRING', $_SERVER) && self::$method === 'GET')
+		{
+			$rawData = json_decode(urldecode($_SERVER['REDIRECT_QUERY_STRING']));
+		}
+		else{
+			$rawData = json_decode(urldecode(file_get_contents('php://input')));
+		}
+		//object {data: {}, resource: null}
+		self::$data = $rawData?->data ? $rawData->data : new \stdClass;
+		self::$resource = $rawData?->resource;
+	}
+	public static function getData()
+	{
+		return self::$data;
+	}
+
+	public static function getResource()
+	{
+		return self::$resource;
+	}
+
+	public static function getHeader()
+	{
+		return self::$header;
+	}
+
+	public static function getToken()
+	{
+		return self::$token;
+	}
+
+	public static function getDecodedToken(){
+		return JWT::decode(self::$token,  Config::JWTKEY);
+	}
+
+}
